f3fa6ac7ec55339488464e0668e2bc3a5c86aad6
Krzysztof Bieliński <krzys9990@gmail.com>
2020-03-22 21:31:11 +0100

    Bug naprawiony

--- wierzcholek.py
+++ wierzcholek.py
@@ -4,9 +4,9 @@
 class Wierzchołek:
     def __init__(self, sąsiedzi=[]):
         # lista sąsiadów w postaci '(id sąsiada, koszt dojazdu do sąsiada)'
-        self.sąsiedzi = sąsiedzi
+        self.sąsiedzi = list(sąsiedzi)
 
 class Graf:
     def __init__(self, wierzchołki={}):
         # słownik wierzchołków w grafie '{id -> Wierzchołek}'
-        self.wierzchołki = wierzchołki
+        self.wierzchołki = dict(wierzchołki)
