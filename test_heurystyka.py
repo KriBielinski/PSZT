29d438b1455bf5574c206f09729a331b54a65f57
Piotr Sawicki <01143606@pw.edu.pl>
2020-03-23 21:51:11 +0100

    Dodano funkcje obliczajaca heurystyke oraz test na jej poprawnosc

---
+++ test_heurystyka.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from wierzcholek import Wierzchołek, Graf
+
+graf = Graf()
+graf.czytaj_plik("wejscie.txt")
+graf.oblicz_heurystykę(id_celu='5')
+
+assert graf.wierzchołki.get('5').heurystyka == 0
+assert graf.wierzchołki.get('3').heurystyka == 1
+assert graf.wierzchołki.get('4').heurystyka == 1
+assert graf.wierzchołki.get('6').heurystyka == 1
+assert graf.wierzchołki.get('2').heurystyka == 3
+assert graf.wierzchołki.get('1').heurystyka == 3
--- wierzcholek.py
+++ wierzcholek.py
@@ -6,6 +6,7 @@ maxZapamiętanychKrawędzi = 100
 class Wierzchołek:
     def __init__(self, sąsiedzi=[]):
         self.sąsiedzi = list(sąsiedzi)
+        self.heurystyka = -1
         
     def dodaj_sąsiada(self, id_celu, koszt):
         self.sąsiedzi.append((id_celu, float(koszt)))
@@ -19,19 +20,52 @@ class Graf:
         if id_swoje not in self.wierzchołki:
             self.wierzchołki[id_swoje] = Wierzchołek()
         self.wierzchołki[id_swoje].dodaj_sąsiada(id_celu, koszt)
+
         if id_celu not in self.wierzchołki:
             self.wierzchołki[id_celu] = Wierzchołek()
         self.wierzchołki[id_celu].dodaj_sąsiada(id_swoje, koszt)
         
     def czytaj_plik(self, nazwa_pliku):
         plik = open(nazwa_pliku)
+
         for linia in plik:
             linia = linia.strip().split()
+
             id_swoje = linia[0]
             id_celu = linia[1]
             koszt = linia[2]
+
             if(len(self.krawędzieMalejąco) < maxZapamiętanychKrawędzi):
                 self.krawędzieMalejąco.append((float)(koszt))
+
             self.dodaj_wierzchołek(id_swoje, id_celu, koszt)
+
         self.krawędzieMalejąco.sort(reverse=True)
         plik.close()
+
+    def oblicz_heurystykę(self, id_celu):
+        aktualnaHeurystyka = 0
+
+        odwiedzone = dict()
+        for id in self.wierzchołki.keys():
+            odwiedzone[id] = False
+
+        wierzchołki = list()
+        wierzchołki.append(id_celu)
+        odwiedzone[id_celu] = True
+
+        while(len(wierzchołki) > 0):
+            wierzchołkiNaNastępnymPoziomie = list()
+
+            for id in wierzchołki:
+                self.wierzchołki.get(id).heurystyka = aktualnaHeurystyka
+                for (sąsiad, koszt) in self.wierzchołki.get(id).sąsiedzi:
+                    if(odwiedzone[sąsiad] == False):
+                        wierzchołkiNaNastępnymPoziomie.append(sąsiad)
+                        odwiedzone[sąsiad] = True
+
+            wierzchołki = wierzchołkiNaNastępnymPoziomie
+
+            aktualnaHeurystyka += self.krawędzieMalejąco[-1]
+            if(len(self.krawędzieMalejąco) > 1):
+                self.krawędzieMalejąco.pop(-1)
