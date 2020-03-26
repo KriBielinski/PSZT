bdf15b2f111beb4a7f3921dc295144755d280d4a
Piotr Sawicki <01143606@pw.edu.pl>
2020-03-26 20:44:27 +0100

    Dodano testy.py jako zbior wszystkich testow

---
+++ testy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from test_astar import test_astar
+from test_bruteforce import test_bruteforce
+from test_dijkstra import test_dijkstra
+from test_heurystyka import test_heurystyka
+from test_wczytywanie import test_wczytywanie
+
+test_wczytywanie()
+test_heurystyka()
+test_dijkstra()
+test_astar()
+test_bruteforce()
