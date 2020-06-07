e77b04754a87ab995c77cf78a70aa95c7eee7339
Piotr Sawicki <01143606@pw.edu.pl>
2020-06-08 00:31:04 +0200

    Wyniki z nowych testow.

--- projekt2/average.py
+++ projekt2/average.py
@@ -4,14 +4,15 @@ average = pandas.read_csv('results/results0.csv', index_col=0).iloc[:,:]
 lines = open('results/cross_validation.txt', "r").readlines()
 cross_validation = float(lines[0])
 
+tests = 25
 i = 1
-while i < 25:
+while i < tests:
     average = average.add(pandas.read_csv('results/results' + str(i) + '.csv', index_col=0).iloc[:,:])
     cross_validation += float(lines[i])
     i += 1
 
-average /= 10
-cross_validation /= 10
+average /= tests
+cross_validation /= tests
 
 average.to_csv('average.csv')
 file = open('average_cv.txt', "w")
